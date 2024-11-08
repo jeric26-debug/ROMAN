68e6eb03b92d23e302e373303156971d61990051
jeric26-debug <jeric.roman670@gmail.com>
2024-11-08 15:29:14 +0000

    Task113

---
+++ Tasksheet113.java
@@ -0,0 +1,15 @@
+public class Tasksheet113 {
+    public static void main(String[] args) {
+        // Declare primitive variables
+       byte zero = 0;      // byte value
+       char H ='H' ;      // char value
+       Short three =3;   // short value
+       int one = 1;     // int value
+      float twoPointZero= 2.0f;  // float value
+      boolean isTrue= true;  //boolean value 
+     // Concatenate the primitives into a String
+    String output=H+""+three+one+one+zero + "w" + zero + "r" + one + "d" + twoPointZero+""+isTrue;
+     // Print the concatenated string
+System.out.println(output);
+    }
+}
